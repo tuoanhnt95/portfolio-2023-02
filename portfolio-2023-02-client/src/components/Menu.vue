0670c83cc7d2acdc0572224834f44c2dde8459db
Oanh Nguyen (Oanh Heck of a Story) <myspamvn@yahoo.com>
2023-05-06 23:36:08 +0900

    feat: add top Menu component

---
+++ portfolio-2023-02-client/src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex fixed top-1 right-2 border border-indigo-500 z-40">
+    <div v-for="(item, i) in menuItems" :key="i" class="m-2">
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const menuItems = [
+  { name: "About me", link: "link" },
+  { name: "Projects", link: "link" },
+  { name: "Contact", link: "link" },
+]  
+</script>
+
+<style scoped>
+</style>
